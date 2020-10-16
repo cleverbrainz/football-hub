59fd7616d607875fab358a3b38f0883a9d08443d
Kenn <kennkenns@hotmail.com>
2020-10-16 13:45:54 +0100

    coach document upload

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,26 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require("firebase-functions");
-const app = require("express")();
+const functions = require('firebase-functions')
+const app = require('express')()
 
-const cors = require("cors");
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 
 // Middleware for authentication
-const authMiddleware = require("./utils/authMiddleware");
+const authMiddleware = require('./utils/authMiddleware')
 const {
   getAllCompanies,
   postNewCompany,
   addNewDetail,
   editCompanyDetail,
   uploadCoachDocument,
-} = require("./utils/controllers/companyController");
+} = require('./utils/controllers/companyController')
 
 const {
   newEnquiry,
   getCustomerEnquiry,
   getCompanyEnquiry,
   updateOneEnquiry,
-} = require("./utils/controllers/enquiryController");
+} = require('./utils/controllers/enquiryController')
 
 const {
   loginUser,
@@ -32,35 +32,35 @@ const {
   forgottenPassword,
   userDocumentUpload,
   initialRegistrationUserInformation,
-} = require("./utils/controllers/userController");
+} = require('./utils/controllers/userController')
 
 // Cloud functios and routes for companies collection
-app.get("/companies", getAllCompanies);
-app.post("/companies", postNewCompany);
-app.post("/companies/:detail", addNewDetail);
-app.patch("/companies/:detail", authMiddleware, editCompanyDetail);
-app.patch("/coaches/:id/document", authMiddleware, uploadCoachDocument);
+app.get('/companies', getAllCompanies)
+app.post('/companies', postNewCompany)
+app.post('/companies/:detail', addNewDetail)
+app.patch('/companies/:detail', authMiddleware, editCompanyDetail)
+app.patch('/coaches/:id/document', authMiddleware, uploadCoachDocument)
 
-app.post("/enquiries", newEnquiry);
-app.get("/enquiries/:company", authMiddleware, getCompanyEnquiry);
-app.get("/enquiries", authMiddleware, getCustomerEnquiry);
+app.post('/enquiries', newEnquiry)
+app.get('/enquiries/:company', authMiddleware, getCompanyEnquiry)
+app.get('/enquiries', authMiddleware, getCustomerEnquiry)
 
-app.patch("/enquiries/:id", updateOneEnquiry);
+app.patch('/enquiries/:id', updateOneEnquiry)
 
 // Cloud functions and routes for user collection
-app.delete("/user/image/:id", authMiddleware, imageDeletion);
-app.post("/user/:id/image", authMiddleware, customerImageUpload);
-app.post("/user/:id/signup", initialRegistrationUserInformation);
+app.delete('/user/image/:id', authMiddleware, imageDeletion)
+app.post('/user/:id/image', authMiddleware, customerImageUpload)
+app.post('/user/:id/signup', initialRegistrationUserInformation)
 
-app.post("/user/document", authMiddleware, userDocumentUpload);
-app.get("/users/:id", getOneUser);
-app.post("/user/:id", authMiddleware, updateUserInformation);
-app.post("/signup", registerUser);
-app.post("/login", loginUser);
+app.post('/user/document', authMiddleware, userDocumentUpload)
+app.get('/users/:id', getOneUser)
+app.post('/user/:id', authMiddleware, updateUserInformation)
+app.post('/signup', registerUser)
+app.post('/login', loginUser)
 
 // app.get('/users', getAllUsers)
-app.post("/resetpassword", forgottenPassword);
+app.post('/resetpassword', forgottenPassword)
 // app.get('/users/:id', getOneUser)
 
 // Configures firebase and lets it know that the app container is serving the functionalities
-exports.api = functions.region("europe-west2").https.onRequest(app);
+exports.api = functions.region('europe-west2').https.onRequest(app)
